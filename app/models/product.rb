189f9ec10b9c2f4b3ae14db93339a9ae33fd70eb
alex <alexrfarnes@gmail.com>
2022-07-20 19:38:14 +0800

    Add validations for price, title and code of product

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,6 +15,33 @@ class Product < ApplicationRecord
     # save
     before_save :validate_product
     after_save :send_notification
+    after_save :push_notification, if: :discount?
+
+    validates :title, presence: { message: "Es necesario definir un valor para el titulo" }
+    validates :code, presence: { message: "Es necesario definir un valor para el codigo" }
+
+    validates :code, uniqueness: { message: "El codigo %{value} ya se encuentra en uso" } # El placeholder %{value} permite pasar el valor del atributo
+
+    # validates :price, length: { minimum: 3, maximum: 10 }
+    validates :price, length: { in: 3..10, message: "El precio se encuentra fuera de rango (Min: 3, Max: 10" }, if: :has_price?
+
+    
+    
+    def total
+        # el precio esta guardado en centavos
+        self.price / 100
+    end
+
+    def discount?
+        self.total <= 5
+    end
+
+    def has_price?
+        # primero chequear que tenga un precio y luego que sea mayor a 0
+        !self.price.nil? && self.price > 0
+    end
+
+    private
 
     def validate_product
         puts "\n\n\n>>>>>> Un nuevo producto sera añadido a almacen!"
@@ -24,9 +51,10 @@ class Product < ApplicationRecord
         puts "\n\n\n>>>>>> Un nuevo producto fue añadido a almacen: #{self.title} - $#{self.total} USD"
     end
 
-    def total
-        # el precio esta guardado en centavos
-        self.price / 100
+    # Si el precio es menor o igual que $5
+    def push_notification
+        puts "\n\n\n>>>>>> Un nuevo producto en descuento se encuentra disponible: #{self.title}"
     end
+
 
 end
